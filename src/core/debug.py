77c4df8fd55769cccd85d6d95736fa6cc236761a
aeim <anexerciseinmental@outlook.com>
2025-09-04 11:48:10 +0100

    aeim

--- src/core/debug.py
+++ src/core/debug.py
@@ -4,11 +4,11 @@ from core.reporting import rangeValueString, vectorString, vectorsString
 isActive = False
 
 
-def outputVectors[T](a: Range[T], b: Range[T]):
-    if isActive:
+def debugVectors[T](a: Range[T], b: Range[T], override: bool = False):
+    if override or isActive:
         print(vectorsString(a, b))
 
 
-def outputValues[T](a: Range[T], b: Range[T]):
-    if isActive:
+def debugValues[T](a: Range[T], b: Range[T], override: bool = False):
+    if override or isActive:
         print(f"{rangeValueString(a)} | {rangeValueString(b)}")
--- src/gen_1/solvers/deductiveResolver.py
+++ src/gen_1/solvers/deductiveResolver.py
@@ -1,5 +1,5 @@
 from core import resolver
-from core.debug import outputValues, outputVectors
+from core.debug import debugValues, debugVectors
 from core.parsers import parseCheck, smartRepartition
 from core.range import Range
 from core.types import CommonRange, CommonalityResult
@@ -7,7 +7,7 @@ from core.types import CommonRange, CommonalityResult
 
 def isCandidate[T](a: Range[T], b: Range[T]) -> bool:
     # customisable: dependent upon kind of problem
-    
+
     isSameSize = a.length == b.length
 
     isCommon = len(a.parts.symmetric_difference(b.parts)) == 0
@@ -17,10 +17,20 @@ def isCandidate[T](a: Range[T], b: Range[T]) -> bool:
 
 def areValid[T](aRanges: list[Range[T]], bRanges: list[Range[T]]):
     # customisable: dependent upon kind of problem
-    
+
     return len(aRanges) > 0 and len(bRanges) > 0
 
 
+# DeductiveResolver (generation 1: general case)
+#
+# The DeductiveResolver recursivly eliminates (solution)-negative-space
+# to reveal common-substrings without "searching"
+# -- (searching: knowing about higher-dimensional structures/ unconditional enumeration of possibility-space) --
+#
+# very much like:
+#   whittling down a piece of wood to reveal the final artefactual-form,
+#   or expert archeological excavation & discovery
+#
 class DeductiveResolver[T](resolver.AbstractResolver[T]):
     commonRanges: list[CommonRange[T]] = []
 
@@ -29,17 +39,25 @@ class DeductiveResolver[T](resolver.AbstractResolver[T]):
 
     def process(self, a: Range[T], b: Range[T], depth: int = 1) -> resolver.AbstractResolver:
 
-        outputVectors(a, b)
+        # debugVectors(a, b)
 
+        # isCandidate: if range constituients and lengths are the same size
+        # parseCheck: check the values by parsing (linear brute-force)
+        # smartRepartitioning: if not, repartition based upon relative common elements (between
+        # each partition's cached valueSet)
+        #
+        # recursion continues for all qualifying pairs of subpartitions
+        #
+        # > note: later generations will natively handle problematic edge-cases
         result = parseCheck(a, b) if isCandidate(a, b) else smartRepartition(a, b)
 
         if result.common:
-            outputValues(a, b)
+            # debugValues(a, b)
 
             # either:
             #   candidate & result of parseCheck, or
             #   non-candidate, but smartRepartitioning led to one-to-one partitions (a == 1 and b == 1), then parsed
-            self.commonRanges.append(result.common)
+            self.add(result.common)
 
         aRanges = result.aRanges  # add filtering here: minimum length, etc
         bRanges = result.bRanges
@@ -48,9 +66,26 @@ class DeductiveResolver[T](resolver.AbstractResolver[T]):
             # fractal recursion:
             #   this process function: 1) attempts parse; 2) repartitions ranges; 3) if partitions are valid, calls processRanges
             #   the processRanges function iteratively cross-checks partitions, by calling this function
+            #
+            # explanation:
+            # on each iteration (for each pair of partitions), the set of elements which drives
+            # repartitioning changes, (the intersect between valueSets is relative to the pair)
+            #
+            # insight:
+            # this approach might seem inefficient due to the way the same ranges are checked and reduced over and over
+            # but at all times, negative-space is being elimintated by nothing other than elementary logic
+            # with no knowledge of the special-domain structure (word-strings in this case), this method will
+            # eliminate all negative-space, leaving only solution positive-space
+            #
+            # > "exclude the impossible and what is left, however improbable, must be the truth"
+            #
+            # the deductiveResolver is therefore, a model elementary logical deduction
             self.processRanges(aRanges, bRanges, depth + 1)
 
             # self.step(len(result.aRanges) + len(result.bRanges))
             # // TODO: need to update this value... ballpark for now; custom data-structures will significanly reduce & custom hardware will eliminate
 
         return self
+
+    def add(self, common: CommonRange[T]):
+        self.commonRanges.append(common)
